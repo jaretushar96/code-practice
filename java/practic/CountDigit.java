8061b0b1ab54ad7dbcf13864e399733c810ad936
Tushar Jare <jaretushar2018@gmail.com>
2025-08-11 23:15:33 +0530

    Count Degits in Number

---
+++ java/practic/CountDigit.java
@@ -0,0 +1,14 @@
+public class CountDigit{
+    public static void main(String[] args) {
+        int num = 9876;
+        int count = 0;
+
+        while (num != 0) {
+            count++;
+            num =num/ 10;
+        }
+
+        System.out.println("Total Digits: " + count);
+    }
+}
+
