99280a923347fa7a7fd7296a757d965af4dee3bf
Tushar Jare <jaretushar2018@gmail.com>
2025-08-14 21:45:18 +0530

    Count Vowels Consonants in string with java

--- java/practic/CountVowelsConsonants.java
+++ java/practic/CountVowelsConsonants.java
@@ -8,7 +8,8 @@ public class CountVowelsConsonants {
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
 
-            // Check if the character is a letter
+          
+            
             if (ch >= 'a' && ch <= 'z') {
                 if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                     vowels++;
