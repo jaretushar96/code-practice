6066f82236cbff8ca672c26d3db6e200ef4cb6a2
Tushar Jare <jaretushar2018@gmail.com>
2025-08-15 23:42:44 +0530

    Find factorial of numbers With java

--- java/practic/Factorial.java
+++ java/practic/Factorial.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Factorial{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of N: ");
+        System.out.println("Enter the value of N: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
