f842b7115a2a73476d28766eb9ec98ab54ef28d3
Tushar Jare <jaretushar2018@gmail.com>
2025-08-15 23:41:20 +0530

    Find factorial of numbers with java

---
+++ java/practic/Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of N: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int fact = 1;
+            for (int j = 1; j <= i; j++) {
+                fact = fact * j;
+            }
+            System.out.println("Factorial of " + i + " is: " + fact);
+        }
+    }
+}
