d06fad659eb425ce1bdcb9e1209ae34809474daa
Tushar Jare <jaretushar2018@gmail.com>
2025-08-17 09:28:21 +0530

    Fibonachi Series Up to N th Term  with java

--- java/practic/Fibonachi.java
+++ java/practic/Fibonachi.java
@@ -3,7 +3,8 @@ public class Fibonachi
     public static void main(String[] args) 
     {
         int a = 0, b = 1, n = 10;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             System.out.print(a + " ");
             int next = a + b;
             a = b;
