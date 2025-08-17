1c36d99298372c5077856ec60f4ed428cb1a53d1
Tushar Jare <jaretushar2018@gmail.com>
2025-08-17 09:27:34 +0530

    Fibonachi Series  with java

--- java/practic/Fibonachi.java
+++ java/practic/Fibonachi.java
@@ -1,6 +1,7 @@
 public class Fibonachi 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int a = 0, b = 1, n = 10;
         for (int i = 0; i < n; i++) {
             System.out.print(a + " ");
