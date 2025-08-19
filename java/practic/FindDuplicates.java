6c994b70b0f522b8fbf9f187852d8153c4b87dc6
Tushar Jare <jaretushar2018@gmail.com>
2025-08-19 15:18:15 +0530

    Find Duplicate Numbers  In  Java

---
+++ java/practic/FindDuplicates.java
@@ -0,0 +1,14 @@
+public class FindDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 3, 5, 2, 1};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate: " + arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
