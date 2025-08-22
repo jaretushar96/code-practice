d6eceb3f362cc5f87ba9948b737e4a8f65e29f54
Tushar Jare <jaretushar2018@gmail.com>
2025-08-22 16:09:49 +0530

    Linear Search

---
+++ java/practic/LinearSearch.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 30, 45, 50};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to search: ");
+        int target = sc.nextInt();
+        int c=0;
+        // boolean found = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Found at index: " + i);
+                // found = true;
+                c++;
+                break;
+            }
+        }
+
+        if (c==0) {
+            System.out.println("Not found");
+        }
+
+        sc.close();
+    }
+}
