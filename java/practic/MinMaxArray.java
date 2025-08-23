2c1552d61119fb1abd02a6723fe8cd6c441d93fc
Tushar Jare <jaretushar2018@gmail.com>
2025-08-23 19:20:47 +0530

    Find Min & Max number form Array

---
+++ java/practic/MinMaxArray.java
@@ -0,0 +1,16 @@
+public class MinMaxArray {
+    public static void main(String[] args) {
+        int[] arr = {12, 5, 8, 1, 19};
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        System.out.println("Smallest: " + min);
+        System.out.println("Largest: " + max);
+    }
+}
