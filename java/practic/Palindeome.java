1a9161020186a1396028a7f31f44b8097f3bfe97
Tushar Jare <jaretushar2018@gmail.com>
2025-08-25 16:58:00 +0530

    Find String is palindrome or Not

--- java/practic/Palindeome.java
+++ java/practic/Palindeome.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Palindeome {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         Scanner sc = new  Scanner(System.in);
         System.out.println("Enter the string :");
 
