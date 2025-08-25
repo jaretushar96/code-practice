a54c9f28f57b990f6cb875667f7498b3bfd7d528
Tushar Jare <jaretushar2018@gmail.com>
2025-08-25 16:58:55 +0530

    Find String is palindrome or Not

--- java/practic/Palindeome.java
+++ java/practic/Palindeome.java
@@ -10,8 +10,10 @@ public class Palindeome {
         
         // boolean isPalindrome = true;
         int c=0;
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+        for (int i = 0; i < str.length() / 2; i++)
+         {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) 
+            {
               
                 c++;
                 // isPalindrome = false;
