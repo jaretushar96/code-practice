fcb8a480cb3ec188eed76dfa363f9f9bf2b0e5b7
Tushar Jare <jaretushar2018@gmail.com>
2025-08-26 16:00:55 +0530

    Find number is perfect or not

---
+++ java/practic/PerfectNumber.java
@@ -0,0 +1,13 @@
+public class PerfectNumber {
+    public static void main(String[] args) {
+        int num = 14;
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0)
+                sum += i;
+        }
+
+        System.out.println(num + (sum == num ? " is a Perfect Number" : " is not a Perfect Number"));
+    }
+}
