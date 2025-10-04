b04bf22ee9f3c1601e234465d9721dd46ffe18af
Tushar Jare <jaretushar2018@gmail.com>
2025-10-04 17:45:24 +0530

    Practic of javascript JS file

---
+++ Apna College/javascript/frist.js
@@ -0,0 +1,49 @@
+console.log("Hello Ahmednager");
+console.log(" infobean");
+
+let pencileP=10;
+let erasorP=5;
+let output=`The price of pencil is: ${pencileP+2}`;
+console.log(output);
+console.log(" The total price of items is ",pencileP+erasorP);
+
+let str="apple";
+if(str[0]=="a"&& str.length>3)
+{
+    console.log(`${str} is a good `);
+}
+else
+{
+    console.log(`${str} is not a good `);
+}
+
+// alert(" Your hotsopt is off Check internet connection ");
+
+ console.error(`this is error  `);
+  console.warn(`this is warning  `);
+
+//   let name =prompt("Enteer your name");
+  console.log(name);
+
+
+
+   let student=["aman","Shraddha","rajat"];
+   console.log(student);
+
+   let info=["sharaddha",23,67.98]
+   console.log(info);
+   info.length;
+
+   let cars=["Bmw","Audi","Xuv","Toyta","RR","alto"];
+    console.log(cars);
+
+    let mon=["january","july","march","augest"];
+        console.log(mon);
+    
+
+        mon.shift();
+           mon.shift();
+         mon.unshift('june');  
+          mon.unshift('july');  
+
+            console.log(mon);
