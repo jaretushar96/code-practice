ffdc48ef5b77d54af459dd83b2ec826cb60d0b5b
Tushar Jare <jaretushar2018@gmail.com>
2025-10-05 16:19:07 +0530

    making todo list program js file

---
+++ Apna College/javascript/todo.js
@@ -0,0 +1,56 @@
+let todo=[];
+
+let choice =prompt("Its To do list enter your choice  ");
+
+while(true){
+
+     if(choice=="exit")
+    {
+        console.log(" exited ");
+        break;
+    }
+     
+    if(choice=="add")
+    {
+       
+      
+          let task= prompt(" Enter work to add");
+
+          todo.push(task);
+          console.log(" Task added");
+        
+    }
+
+    else if(choice =="print")
+    {
+            console.log("---------");
+     for(let i=0;i<todo.length;i++)
+        {
+        
+
+           console.log(i,todo[i]);
+
+           
+        }
+         console.log("---------");
+    }
+
+
+    else if(choice=="delate")
+    {
+        let ind=prompt(" please enter the index for delet ");
+
+        todo.splice(ind,1);
+         console.log(" deleated....");
+    }
+
+   
+    else{
+        console.log("Wrong choice ");
+    }
+
+
+
+
+
+}
