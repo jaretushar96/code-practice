595e13442f3072aa05baf27c7e3ef1dbd59eeede
Tushar Jare <jaretushar2018@gmail.com>
2025-10-06 19:37:40 +0530

    practic of javascript

---
+++ Apna College/javascript/second.js
@@ -0,0 +1,43 @@
+
+// literals
+let student={
+    name : "Tushar",
+    age : 22,
+    marks:94
+
+}
+
+// Object
+let stu2=["tushar",22,94];
+
+// creating post
+
+const post={
+    username :"@tusharjare",
+    content :"this is my 1 st post ",
+    likes : 150,
+    repost : 3,
+    tagged :["@coding","java Developer"]
+}
+
+console.log(post);
+console.log(post.content);     
+console.log(post["content"]); 
+
+// how to update value of key in object
+post.likes=200;
+post.repost="five";
+post.tagged.push("@xyz","@Google");
+post.gender="Male";   // Add new value in object
+post.city="Pune"; 
+console.log(post);
+
+delete post.tagged[2];
+delete post.gender;
+
+console.log(post);
+console.log(post.tagged);
+console.log(post.tagged[2]);
+
+
+
