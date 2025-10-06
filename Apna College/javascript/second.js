8a99cfef6293f5ba36ca18a9cd214631ce2fc9ff
Tushar Jare <jaretushar2018@gmail.com>
2025-10-06 19:38:40 +0530

    practic of javascript

--- Apna College/javascript/second.js
+++ Apna College/javascript/second.js
@@ -40,4 +40,47 @@ console.log(post.tagged);
 console.log(post.tagged[2]);
 
 
+//  nested object
+
+const stud =
+{
+
+    tushar :
+    {
+        surname :"Jare",
+        city : "pune",
+        rollNo :51,
+        marks :[99,98,88,96]
+
+    },
+    dipak :
+    {
+        surname :"Chande",
+        city : "Shevgaon",
+        rollNo :55,
+
+    },
+    visvas :
+    {
+
+        surname :"Alhat",
+        city : "Nager",
+        rollNo :22,
+    }
+
+}
+
+console.log(stud);
+console.log(stud.tushar);
+console.log(stud.tushar.surname);
+
+stud.tushar.marks.push(97,95,94);
+stud.tushar.rollNo=10;
+delete stud.tushar.marks[2];
+delete stud.tushar.city;
+
+
+console.log(stud.tushar.marks);
+console.log(stud.tushar);
+
 
