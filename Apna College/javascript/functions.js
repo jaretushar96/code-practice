f6a1495437e2d6da09d3f8b20fb58bc73b63bfa0
Tushar Jare <jaretushar2018@gmail.com>
2025-10-07 18:41:39 +0530

    Practic of Function in Javascript Js file

---
+++ Apna College/javascript/functions.js
@@ -0,0 +1,160 @@
+function multiple(func, count)
+{
+    for(let i=1;i<=count;i++)
+    {
+        func();
+    }
+}
+    let greet= function()
+    {
+        console.log("hello");
+    }
+
+
+multiple(greet,100);
+
+
+
+
+function oddEvenFactory(req)
+{
+
+
+if( req=="odd")
+{
+let odd= function(n)
+{
+    console.log(!(n%2==0));
+}
+return odd;
+}
+
+else if(req=="even")
+{
+    let even= function(n)
+{
+    console.log((n%2==0));
+}
+return even;
+}
+
+
+}
+
+let fu=oddEvenFactory("odd");
+fu(10);
+
+
+
+// methods ( function in object )
+
+
+      console.log(" this is example of Methods in object ");
+const calculator ={
+     add: function(a,b)
+     {
+        return a+b;
+     },
+     sub: function(a,b)
+     {
+        return a-b;
+     },
+     mult: function(a,b)
+     {
+        return a*b;
+     },
+    }
+
+    console.log(calculator.add(4,5));
+     console.log(calculator.sub(14,9));
+
+
+
+//  same but in different way 
+const calculator1 ={
+     add(a,b)
+     {
+        return a+b;
+     },
+     sub(a,b)
+     {
+        return a-b;
+     },
+     mult (a,b)
+     {
+        return a*b;
+     },
+    }
+
+    console.log(calculator.add(9,5));
+    console.log(calculator.sub(14,3));
+
+     
+
+     //  Arrows Functions
+
+
+      console.log(" this is example of arrow fun");
+     let power=(a,b)=>
+     {
+        return a**b;
+     }
+
+     let power1=(a,b)=>a**b    // when we want to return value we can do like this 
+     
+
+      let name1=str=>     // when only 1 argument not need to give ()
+     {
+        console.log(str);
+     }
+
+      let name2= () =>     // when No argument pass , need to give ()
+     {
+        console.log("jare");
+     }
+
+     console.log(power(2,3));
+       console.log(power1(2,4));
+     name1("Tushar");
+     name2();
+
+
+
+//   SetTimeout Function
+
+console.log(" hii ");
+
+setTimeout( ()=> 
+{
+    console.log(" Tushar ");
+},4000)
+
+console.log(" Welcome");
+
+
+
+console.log(" hello ");
+
+setTimeout( function greed9 ()
+ {
+    console.log(" Dipak ");
+},4000)
+
+console.log(" Welcome");
+
+
+
+
+// Set interval 
+// excute fun againg & agin after time 
+
+
+console.log(" Example of set interval ");
+
+
+console.log(" Welcome ");
+
+setInterval( ()=> 
+{
+    console.log(" Developer  ");
+},100000)
