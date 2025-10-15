319b1e9a492e54782c06cbcbb6e0f9a64bf2f5c6
Tushar Jare <jaretushar2018@gmail.com>
2025-10-15 18:30:44 +0530

    Composition (Association)

---
+++ Infobean/java/Inheratance/CompositionEx.java
@@ -0,0 +1,78 @@
+class camera
+{
+   private int resolation;
+     private int megapixal;
+
+    camera(int reso,int mega) {
+        resolation=reso;
+        megapixal=mega;
+    }
+
+    void show()
+    {
+        System.out.println("Description of Camera  ");
+        System.out.println("Resolation :"+resolation+" p\nMegapixele :"+megapixal+"Mp\n-------------------------------");
+    }
+}
+
+class processor
+{
+    
+   private String probrand;
+   private String core;
+   private int frequency;
+
+    processor(String brand, String core,int frequency)
+    {
+    this.probrand=brand;
+  this.core=core;
+  this.frequency=frequency;
+
+    }
+
+    void show()
+    {
+        System.out.println(" Description of Processor  ");
+        System.out.println("Breand :"+probrand+"\nCore :"+core+"\nFrequency :"+frequency+"GHz");
+       
+    }
+
+}
+class smartPhone
+{
+   private camera cam;
+   private processor pro;
+ private String brand;
+   private String model;
+   private int price;
+
+    smartPhone(String brand, String model,int price)
+    {
+  this.brand=brand;
+  this.model=model;
+  this.price=price;
+  this.pro=new processor("Dragon","xy34",99);
+  this.cam=new camera(1080, 64);
+    }
+
+     void show()
+    {
+        System.out.println("--------------------------------\nDescription of Smartphone  ");
+        System.out.println("Brand :"+brand+"\nModel :"+model+"\nPrice :"+price);
+        pro.show();
+         cam.show();
+    }
+
+
+}
+
+
+
+public class CompositionEx {
+    
+
+    public static void main(String[] args) {
+        smartPhone sm=new smartPhone("Samsung", "S-23 Pro" , 56000);
+        sm.show();
+    }
+}
