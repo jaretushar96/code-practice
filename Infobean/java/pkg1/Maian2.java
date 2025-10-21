271413c35cca9800fb8d82e7284e76f40827bab0
Tushar Jare <jaretushar2018@gmail.com>
2025-10-21 20:37:05 +0530

    Aggergation (Main method)

---
+++ Infobean/java/pkg1/Maian2.java
@@ -0,0 +1,16 @@
+public class Maian2 {
+    
+
+    public static void main(String[] args) {
+        
+        EmpLib el=new EmpLib("Vivek","Radha");
+        Book b=new Book("OS","Math");
+
+
+        b.show();
+        el.show();
+
+
+
+    }
+}
