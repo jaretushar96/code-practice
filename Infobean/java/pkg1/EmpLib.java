7e08be68a7967b6fb33b6b4dcb54cfd657afb85a
Tushar Jare <jaretushar2018@gmail.com>
2025-10-22 23:20:09 +0530

    Employeee oc Library  class

---
+++ Infobean/java/pkg1/EmpLib.java
@@ -0,0 +1,21 @@
+public class EmpLib {
+    
+  String em1;
+   String em2;
+
+  
+
+     EmpLib(String em1, String em2) 
+     {
+         this.em1=em1;
+         this.em2=em2; 
+    }
+
+    void show()
+    {
+
+          System.out.println("Employees  :"+em1+"  "+em2);
+    }
+   
+
+}
