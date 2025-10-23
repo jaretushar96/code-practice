09ae48f32a423683bb0626daa5144ee048c01e8b
Tushar Jare <jaretushar2018@gmail.com>
2025-10-23 19:06:59 +0530

    Area

--- Infobean/java/Staticexample/Area.java
+++ Infobean/java/Staticexample/Area.java
@@ -1,5 +1,4 @@
 package Staticexample;
-import java.util.*;
 import java.util.Scanner;
 public class Area {
     
@@ -15,7 +14,7 @@ public class Area {
         return(pi*r);
   }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
  Scanner sc= new Scanner(System.in);
 
 
