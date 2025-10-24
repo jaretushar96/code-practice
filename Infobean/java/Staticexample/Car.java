89b04198a46b64650720c8e71faff08c66b74bfd
Tushar Jare <jaretushar2018@gmail.com>
2025-10-24 09:11:01 +0530

    Car class

---
+++ Infobean/java/Staticexample/Car.java
@@ -0,0 +1,23 @@
+package Staticexample;
+
+public class Car {
+    
+static String brand="BMW";
+int cost;
+String colour;
+
+void info(String colour, int cost)
+{
+
+System.out.println(colour+" "+ cost+" "+ brand);
+}
+
+public static void main(String [] args)
+{
+
+    Car c= new Car();
+    c.info("Red",2000);
+
+}
+
+}
