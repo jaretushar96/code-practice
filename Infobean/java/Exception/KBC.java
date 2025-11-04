c20dd45fe973ed7512c21ff2e1daccdf045da852
Tushar Jare <jaretushar2018@gmail.com>
2025-11-04 18:31:45 +0530

    Exception Handling With game like KBC

--- Infobean/java/Exception/KBC.java
+++ Infobean/java/Exception/KBC.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class KBC
 {
+
+        
     public static void main(String[] args)
     {
-      System.out.println("Helllo Tushar");
+        Scanner sc=new Scanner(System.in);
+             
+      System.out.println("Enter the Number");
+      int a=sc.nextInt();
+      System.out.println("Your number is "+a);
     }
 }
