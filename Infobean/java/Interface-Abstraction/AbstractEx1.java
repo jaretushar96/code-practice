a4ad6e72882a8d42a2aca977037d056b71721217
Tushar Jare <jaretushar2018@gmail.com>
2025-11-09 09:43:17 +0530

    Abstraction Exaample

---
+++ Infobean/java/Interface-Abstraction/AbstractEx1.java
@@ -0,0 +1,82 @@
+package Abstractio_Interfaces;
+
+abstract class phone
+{
+	abstract void calling();
+	
+	abstract void camera();
+	abstract void torch();
+	
+}
+
+
+
+class oppo extends phone
+{
+
+	
+	void calling() {
+		System.out.println("\n ------------------------------------------\n IN OPPO PHONE\n");	
+	System.out.println(" Calling Function is Avilable ");	
+	}
+
+	
+	void camera() {
+		
+		System.out.println(" Camera Function is Avilable ");	
+	}
+
+	
+	void torch() {
+		System.out.println(" torch Function is Avilable ");	
+	}
+	
+	void gps() {
+		System.out.println(" GPS Function is Avilable ");	
+	}
+	
+}
+
+class vivo extends phone
+{
+
+	
+	void calling() {
+		System.out.println(" \n ------------------------------------------\n IN VIVO PHONE \n");	
+	System.out.println(" Calling Function is Avilable ");	
+	}
+
+	
+	void camera() {
+		
+		System.out.println(" Camera Function is Avilable ");	
+	}
+
+	
+	void torch() {
+		System.out.println(" torch Function is Avilable ");	
+	}
+	
+	void gaming() {
+		System.out.println(" Gaming Function is Avilable ");	
+	}
+	
+}
+
+public class AbstractEx1 {
+
+	public static void main(String [] args)
+	{
+		vivo v=new vivo();
+		v.calling();
+		v.camera();
+		v.torch();
+		v.gaming();
+		oppo o=new oppo();
+		o.calling();
+		o.camera();
+		o.torch();
+		o.gps();
+	}
+	
+}
