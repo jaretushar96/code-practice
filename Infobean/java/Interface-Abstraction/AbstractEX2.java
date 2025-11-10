21d0b6a7766f3e1825471c9fe5647e8fbc951c8b
Tushar Jare <jaretushar2018@gmail.com>
2025-11-10 20:12:51 +0530

    Abstraction

---
+++ Infobean/java/Interface-Abstraction/AbstractEX2.java
@@ -0,0 +1,37 @@
+package Abstractio_Interfaces;
+
+abstract class pen
+{
+	abstract void write();
+	abstract void refil();
+	
+}
+class FountainPen extends pen
+{
+
+	@Override
+	void write() {
+		System.out.println(" Write method");
+		
+	}
+
+	@Override
+	void refil() {
+		System.out.println(" Refil method");
+	}
+	
+	
+	void ink() {
+		System.out.println(" Red ink method");
+	}
+}
+
+public class AbstractEX2 {
+	public static void main(String [] args)
+	{
+		FountainPen f=new FountainPen();
+		f.write();
+		f.refil();
+		f.ink();
+	}
+}
