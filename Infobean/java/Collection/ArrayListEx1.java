95440de6564b5f65df3dfc4ef719cab0d4848763
Tushar Jare <jaretushar2018@gmail.com>
2025-11-12 19:34:51 +0530

    ArrayList

---
+++ Infobean/java/Collection/ArrayListEx1.java
@@ -0,0 +1,20 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListEx1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> abc =new ArrayList<Integer>();
+        abc.add(10);
+          abc.add(8);
+            abc.add(50);
+              abc.add(44);
+                abc.add(22);
+                abc.addFirst(999);
+
+                System.out.println(abc);
+                Collections.sort(abc);
+                System.out.println(abc);
+        
+    }
+}
