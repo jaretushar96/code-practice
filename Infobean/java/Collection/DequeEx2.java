68dafa79efde28fd2855592b99800dfe75ded2d2
Tushar Jare <jaretushar2018@gmail.com>
2025-11-16 11:57:59 +0530

    Deque

--- Infobean/java/Collection/DequeEx2.java
+++ Infobean/java/Collection/DequeEx2.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class DequeEx2 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         String sentence = sc.nextLine();
