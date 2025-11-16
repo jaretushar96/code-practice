4ab92f701f50aeea8e6122e52bc17d2c0a43676f
Tushar Jare <jaretushar2018@gmail.com>
2025-11-16 11:56:55 +0530

    Deque

---
+++ Infobean/java/Collection/DequeEx2.java
@@ -0,0 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class DequeEx2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        Deque<String> dq = new ArrayDeque<>();
+        for (String word : sentence.split(" ")) dq.push(word);
+
+        System.out.print("Reversed Sentence: ");
+        while (!dq.isEmpty()) System.out.print(dq.pop() + " ");
+    }
+}
