178f53bda469d76d04280db326b73451a7d26503
Tushar Jare <jaretushar2018@gmail.com>
2025-11-18 23:11:07 +0530

    HashSet

--- Infobean/java/Collection/HashSetEx1.java
+++ Infobean/java/Collection/HashSetEx1.java
@@ -2,7 +2,8 @@ import java.util.HashSet;
 public class HashSetEx1
  {
     
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         HashSet<String> countries = new HashSet<>();
         countries.add("India");
         countries.add("USA");
