08fc8b9db79e9699816325a9bd47d7fc83876c89
Tushar Jare <jaretushar2018@gmail.com>
2026-01-29 20:58:40 +0530

    MergeFilesChar class

--- Infobean/java/FileInputOutput/MergeFilesChar.java
+++ Infobean/java/FileInputOutput/MergeFilesChar.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 public class MergeFilesChar
  {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         try (FileWriter fw = new FileWriter("merged.txt");
              FileReader fr1 = new FileReader("file1.txt");
