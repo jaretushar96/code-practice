b9c69323d885bb0ea535119e9eb0b641fc39088b
Tushar Jare <jaretushar2018@gmail.com>
2026-02-13 14:56:14 +0530

    ReadStringBuffer class

---
+++ Infobean/java/FileInputOutput/ReadStringBuffer.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadStringBuffer {
+    public static void main(String[] args) {
+
+        StringBuffer sb = new StringBuffer();
+        
+
+        try  {
+                BufferedReader br = new BufferedReader(new FileReader("demo.txt"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+            //    sb.append(line).append("\n");
+             System.out.println(line);
+            }
+              
+            br.close(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // System.out.println("File Content:");
+        // System.out.println(sb.toString());
+
+        
+    }
+}
