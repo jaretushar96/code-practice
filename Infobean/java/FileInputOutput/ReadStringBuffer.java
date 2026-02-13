ca365dd68ce4ef97db0d0006b94b1409dda28792
Tushar Jare <jaretushar2018@gmail.com>
2026-02-13 14:57:02 +0530

    ReadStringBuffer class

--- Infobean/java/FileInputOutput/ReadStringBuffer.java
+++ Infobean/java/FileInputOutput/ReadStringBuffer.java
@@ -4,7 +4,8 @@ import java.io.IOException;
 
 public class ReadStringBuffer 
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
         StringBuffer sb = new StringBuffer();
         
