510afbb6fc846a53a54ccb609707f7071c3d2066
Tushar Jare <jaretushar2018@gmail.com>
2026-02-14 22:27:14 +0530

    StringBuffEx1 class

--- Infobean/java/FileInputOutput/StringBuffEx1.java
+++ Infobean/java/FileInputOutput/StringBuffEx1.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class StringBuffEx1
  {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
 
      Scanner sc=new Scanner(System.in);
 
