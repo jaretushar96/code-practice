ca1fa937fcc00aa72725cbee46f56377fc5009be
Tushar Jare <jaretushar2018@gmail.com>
2026-02-14 22:25:57 +0530

    StringBuffEx1 class

---
+++ Infobean/java/FileInputOutput/StringBuffEx1.java
@@ -0,0 +1,26 @@
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class StringBuffEx1 {
+    public static void main(String[] args) {
+
+     Scanner sc=new Scanner(System.in);
+
+        try  {
+              FileWriter fw = new FileWriter("demo.txt");
+               System.out.println("Enter text to write in file:");
+            String data = sc.nextLine();
+            fw.write(data);
+              fw.close(); 
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+    
+
