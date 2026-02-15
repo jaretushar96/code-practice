3eb5e875d38a6d638229fb15d188bde472efccc8
Tushar Jare <jaretushar2018@gmail.com>
2026-02-15 12:53:11 +0530

    EmployeePerformance class

---
+++ Infobean/java/LembdaFun/EmployeePerformance.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Scanner;
+class Employee{
+    String name;
+    int age;
+
+    long salary;
+    int performance;
+
+    public Employee(String name, int age, long salary, int performance) {
+        this.name = name;
+        this.age = age;
+       
+        this.salary = salary;
+        this.performance = performance;
+    }
+
+
+    
+
+}
+
+public class EmployeePerformance {
+   public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    HashMap <String,Employee> emp1=new HashMap<>();
+    
+    System.out.println("Enter  Name :");
+    String name=sc.nextLine();
+
+      System.out.println("Enter  Age :");
+    int age=sc.nextInt();
+
+     System.out.println("Enter  Salary :");
+    long salary=sc.nextInt();
+
+
+
+      
+
+   } 
+}
